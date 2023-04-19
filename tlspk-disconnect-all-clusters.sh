77de3cf512800e292456b463341a8d6c3d09120d
Alan McGinlay <amcginlay@gmail.com>
2023-04-19 20:29:55 +0200

    enhanced error content

--- tlspk-disconnect-all-clusters.sh
+++ tlspk-disconnect-all-clusters.sh
@@ -36,26 +36,12 @@ check-vars() {
   fi
 }
 
-# DON'T DELETE THIS UNTIL IT'S GRAFTED ACROSS TO tlspk-helper
-# Q. SHOULD WE PASS CONTENTS OF ${temp_dir}/token.out TO log-error ??? also same for disconnect-all-clusters
-# get-oauth-token() {
-#   http_code=$(curl --no-progress-meter -L -w "%{http_code}" -o ${temp_dir}/token.out \
-#     -X POST https://auth.jetstack.io/oauth/token \
-#     --data "audience=https://preflight.jetstack.io/api/v1" \
-#     --data "client_id=jmQwDGl86WAevq6K6zZo6hJ4WUvp14yD" \
-#     --data "grant_type=password" \
-#     --data "username=${TLSPK_SA_USER_ID}" \
-#     --data-urlencode "password=${TLSPK_SA_USER_SECRET}")
-#   if grep -qv "^2" <<< ${http_code}; then log-error "https://auth.jetstack.io/oauth/token [POST] failed with HTTP status code ${http_code}"; fi
-#   cat ${temp_dir}/token.out
-# }
-
 disconnect-all-clusters() {
-  log-info "Diconnecting all clusters from ${TLSPK_ORG}"
+  log-info "Disconnecting all clusters from ${TLSPK_ORG}"
   http_code=$(curl --no-progress-meter -L -w "%{http_code}" -o ${temp_dir}/clusters.out \
     -X GET https://platform.jetstack.io/api/v1/org/${TLSPK_ORG}/clusters \
     --header "authorization: Bearer ${TLSPK_ADMIN_TOKEN}")
-  if grep -qv "^2" <<< ${http_code}; then log-error "https://platform.jetstack.io/api/v1/org/${TLSPK_ORG}/clusters [GET] failed with HTTP status code ${http_code}"; fi
+  if grep -qv "^2" <<< ${http_code}; then log-error "https://platform.jetstack.io/api/v1/org/${TLSPK_ORG}/clusters [GET] failed with HTTP status code ${http_code} and response '$(cat ${temp_dir}/clusters.out)'"; fi
   clusters=($(jq '.[].cluster' --raw-output < ${temp_dir}/clusters.out))
   for cluster in "${clusters[@]}"; do
     log-info "Deleting ${cluster}"
@@ -63,7 +49,7 @@ disconnect-all-clusters() {
         -X DELETE https://platform.jetstack.io/api/v1/org/${TLSPK_ORG}/clusters/${cluster} \
         --header "authorization: Bearer ${TLSPK_ADMIN_TOKEN}")
     cat ${temp_dir}/deletion-${cluster}.out
-    if grep -qv "^2" <<< ${http_code}; then log-error "https://platform.jetstack.io/api/v1/org/${TLSPK_ORG}/clusters/${cluster} [DELETE] failed with HTTP status code ${http_code}"; fi
+    if grep -qv "^2" <<< ${http_code}; then log-error "https://platform.jetstack.io/api/v1/org/${TLSPK_ORG}/clusters/${cluster} [DELETE] failed with HTTP status code ${http_code} and response '$(cat ${temp_dir}/deletion-${cluster}.out)'"; fi
   done
 }
 
